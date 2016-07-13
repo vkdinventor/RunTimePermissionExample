3e9bd01a0eba0b7dcf0d1ea09a4940319510aa9e
vkdinventor <vkdinventor@gmail.com>
2016-07-13 15:28:29 +0530

    falatu line removed in MainActivity.java

--- app/src/main/java/com/vikash/example/runtimepermissionexample/MainActivity.java
+++ app/src/main/java/com/vikash/example/runtimepermissionexample/MainActivity.java
@@ -48,7 +48,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Log.v("TAG onCreat", "onCreatecalled");
 
 
