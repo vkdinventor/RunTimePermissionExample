4315f161488404250b7816ddbf5bc572bbaf7e03
vikash kumar verma <vkdinventor@gmail.com>
2017-11-01 16:08:04 +0530

    snakbar added to ask permission again when denied

--- app/src/main/java/com/vikash/example/runtimepermissionexample/MainActivity.java
+++ app/src/main/java/com/vikash/example/runtimepermissionexample/MainActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -17,13 +19,12 @@ public class MainActivity extends AppCompatActivity {
     final int MY_PERMISSIONS_REQUEST_CALL = 103;
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[],@NonNull int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_CALL: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
                     // permission was granted, yay! Do the
                     // contacts-related task you need to do.
                     Log.v("TAG result", "Request Premission gramted");
@@ -57,14 +58,26 @@ public class MainActivity extends AppCompatActivity {
         Log.v("TAG ", "call me called");
         if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE)
                 != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.CALL_PHONE},
-                    MY_PERMISSIONS_REQUEST_CALL);
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.CALL_PHONE)) {
+                //show some ui
+                Snackbar.make(v,"This is required to make call",Snackbar.LENGTH_LONG).setAction("OK", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{Manifest.permission.CALL_PHONE},
+                                MY_PERMISSIONS_REQUEST_CALL);
+                    }
+                }).show();
+            } else {
+                Log.d("TAG","requesting permission");
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.CALL_PHONE},
+                        MY_PERMISSIONS_REQUEST_CALL);
+            }
         } else {
             dialNo();
 
         }
-
     }
 
     private void dialNo() {
